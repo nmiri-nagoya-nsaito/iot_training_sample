b7485ca291b08ff95dc429293f8c790b78e6e269
Naoki Saito <saito.naoki@nmiri.city.nagoya.jp>
2017-02-02 20:13:51 +0900

    modify setup script

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+##### functions
+
 function led_turnon () {
     gpio -g mode 21 out
     gpio -g write 21 1
@@ -9,20 +11,121 @@ function led_turnon () {
 }
 
 function led_blynk () {
-    for i in {1..10};do $(led_turnon $1; sleep $1);done;
+    for i in {1..100};do $(led_turnon $1; sleep $1);done;
     echo setup failed.
 }
 
-# when an error occurs, blynk a led 10 times
+##### error handling
+
+# when an error occurs, blynk a led 100 times
 trap 'led_blynk 0.2' ERR
 
+##### Start
 echo 'Setup start.'
 
-DEBIAN_FRONTEND=noninteractive sudo apt-get update
-DEBIAN_FRONTEND=noninteractive sudo -E apt-get -y upgrade
+### update packages
+cd $HOME
+sudo DEBIAN_FRONTEND=noninteractive apt-get update
+sudo DEBIAN_FRONTEND=noninteractive APT_LISTCHANGES_FRONTEND=none apt-get -y upgrade
 
-# when a setup complete successfully, a LED is turned on for 5 seconds.
-led_turnon 5
+### install some packages
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y expect
+
+### install tightvncserver
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y tightvncserver
+
+### set vncserver password
+/usr/bin/expect <<EOF
+spawn "/usr/bin/vncpasswd"
+expect "Password:"
+send "iot0x0x\r"
+expect "Verify:"
+send "$mypass\r"
+expect "view-only password (y/n)?"
+send "n\r"
+expect eof
+exit
+EOF
+
+### some settings to run tightvncserver automatically at the boot time
+cd $HOME
+
+## create vncboot ##
+cat << EOS >> vncboot
+#! /bin/sh
+# /etc/init.d/vncboot
+
+### BEGIN INIT INFO
+# Provides: vncboot
+# Required-Start: $remote_fs $syslog
+# Required-Stop: $remote_fs $syslog
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+# Short-Description: Start VNC Server at boot time
+# Description: Start VNC Server at boot time.
+### END INIT INFO
+
+USER=pi
+HOME=/home/pi
+
+export USER HOME
+
+case "$1" in
+start)
+echo "Starting VNC Server"
+#Insert your favoured settings for a VNC session
+su $USER -c '/usr/bin/vncserver :1'
+;;
+
+stop)
+echo "Stopping VNC Server"
+su $USER -c '/usr/bin/vncserver -kill :1'
+;;
+
+*)
+echo "Usage: /etc/init.d/vncboot {start|stop}"
+exit 1
+;;
+esac
+
+exit 0
+EOS
+## create vncboot end ##
+
+sudo chown root:root vncboot
+sudo chmod 755 vncboot
+sudo cp -a vncboot /etc/init.d
+sudo update-rc.d vncboot defaults
+
+### update Node-RED package
+cd $HOME
+curl -sL https://raw.githubusercontent.com/node-red/raspbian-deb-package/master/resources/update-nodejs-and-nodered -o update-nodejs-and-nodered
+echo "y" | bash update-nodejs-and-nodered
+sudo systemctl enable nodered.service
+
+### install BCM2835 library
+cd $HOME
+wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.51.tar.gz
+tar xvf  bcm2835-1.51.tar.gz
+cd bcm2835-1.51
+./configure
+make
+sudo make check
+sudo make install
+
+### install node-dht-sensor
+sudo npm install -g node-dht-sensor
+
+### install node-red-contrib-dht-sensor
+sudo npm install -g node-red-contrib-dht-sensor
+
+### install node-red-contrib-ui
+sudo npm install -g node-red-contrib-ui
+
+##### complete
+
+# when a setup complete successfully, a LED is turned on for 30 seconds.
+led_turnon 30
 
 echo 'Setup completed.'
 exit 0
